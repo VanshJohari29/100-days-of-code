812bcbdef6f5bc95b3e45af031421d7e18ce874e
Vansh Johari <71847743+VanshJohari29@users.noreply.github.com>
2023-10-16 23:40:46 +0530

    Create 1941.cpp

---
+++ 1941.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        unordered_map<char, int> m;
+    for (int i = 0; i < s.size(); i++)
+        m[s[i]]++;
+
+    int count = m[s[0]];
+    for(auto e:m)
+        if(e.second != count)
+            return false;
+            
+    return true;
+    }
+};
