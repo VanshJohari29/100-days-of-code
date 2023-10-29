38cdeb936b6447c4f68eab4f67ad5e38f3619884
Vansh Johari <71847743+VanshJohari29@users.noreply.github.com>
2023-10-29 22:25:27 +0530

    Create 2418.cpp

---
+++ 2418.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int,string> mpp;
+    
+    for(int i=0;i<names.size();i++){
+        mpp[heights[i]]=names[i];
+    }
+    
+    sort(heights.rbegin(),heights.rend());
+    vector<string>ans;
+    for(int i=0;i<heights.size();i++){
+        ans.push_back(mpp[heights[i]]);
+    }
+    
+    return ans;
+    }
+};
